b2f28c97e398d278893e0620e1ae4693be82f4bb
Anas <anastiour@gmail.com>
2022-05-26 16:26:27 +0100

    Install and use Django Softdelete in models

--- tutorial/models.py
+++ tutorial/models.py
@@ -1,9 +1,9 @@
 from django.db import models
+from softdelete.models import SoftDeleteObject
 
-
-class Company(models.Model):
+class Company(SoftDeleteObject, models.Model):
     name = models.CharField(max_length=255)
 
-class Employee(models.Model):
+class Employee(SoftDeleteObject, models.Model):
     name = models.CharField(max_length=255)
     company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True, blank=True)
